2a8b80a3e74901d59353bbc0590f9590964148d3
abdu07aa-cmyk <abdu07.aa@gmail.com>
2026-08-21 14:10:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const CONFIG = {
 
   // GANTI dengan info rekening/e-wallet asli toko kamu.
   PAYMENT_INFO: [
-    { label: 'GoPay', value: '0897-3488-963 a.n. MBUN COLLECTION' },
-    { label: 'Transfer BCA', value: '(isi nomor rekening BCA di sini) a.n. MBUN COLLECTION' },
+    { label: 'GoPay', value: '0897-9502-611 a.n. ABDUL AZIZ' },
+    { label: 'Transfer BCA', value: '(8415597980) a.n. UMMI FATMAH' },
   ],
 
   STORAGE_KEYS: {
